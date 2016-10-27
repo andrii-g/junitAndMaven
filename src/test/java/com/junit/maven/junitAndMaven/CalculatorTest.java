3afef10f5d7b46d0255b0d6a84bfe0055f08eb8a
Andrii_Grygoruk <andrii.grygoruk@racingpost.com>
2016-10-27 16:45:19 +0300

    added calculator properties file

--- src/test/java/com/junit/maven/junitAndMaven/CalculatorTest.java
+++ src/test/java/com/junit/maven/junitAndMaven/CalculatorTest.java
@@ -34,138 +34,177 @@ public class CalculatorTest {
 
     @Category({CalculatorTestSuite.class, SmokeTestsSuite.class})
     @Test
-    public void sumDoubleTest() {
-        final double DELTA = 1e-15;
-        assertEquals("Expected value is not equal to actual", 5.67, calculator.sum(2.65, 3.02), DELTA);
+    public void sumDoubleTest() throws IOException {
+        final double DELTA = Double.parseDouble(resourcesReader.getCalculatorArgument("delta"));
+        double expectedResult = Double.parseDouble(resourcesReader.getCalculatorArgument("sumDoubleTestExpected"));
+        double arg1 = Double.parseDouble(resourcesReader.getCalculatorArgument("sumDoubleTestArg1"));
+        double arg2 = Double.parseDouble(resourcesReader.getCalculatorArgument("sumDoubleTestArg2"));
+        assertEquals("Expected value is not equal to actual", expectedResult, calculator.sum(arg1, arg2), DELTA);
     }
 
     @Category({CalculatorTestSuite.class, SmokeTestsSuite.class})
     @Test
-    public void subLongTest() {
-        assertEquals("Expected value is not equal to actual", 2147483648L, calculator.sub(4294967296L, 2147483648L));
+    public void subLongTest() throws IOException {
+        long expectedResult = Long.parseLong(resourcesReader.getCalculatorArgument("subLongTestExpected"));
+        long arg1 = Long.parseLong(resourcesReader.getCalculatorArgument("subLongTestArg1"));
+        long arg2 = Long.parseLong(resourcesReader.getCalculatorArgument("subLongTestArg2"));
+        assertEquals("Expected value is not equal to actual", expectedResult, calculator.sub(arg1, arg2));
     }
 
     @Category({CalculatorTestSuite.class, SmokeTestsSuite.class})
     @Test
-    public void subDoubleTest() {
-        final double DELTA = 1e-15;
-        assertEquals("Expected value is not equal to actual", -0.37, calculator.sub(2.65, 3.02), DELTA);
+    public void subDoubleTest() throws IOException {
+        final double DELTA = Double.parseDouble(resourcesReader.getCalculatorArgument("delta"));
+        double expectedResult = Double.parseDouble(resourcesReader.getCalculatorArgument("subDoubleTestExpected"));
+        double arg1 = Double.parseDouble(resourcesReader.getCalculatorArgument("subDoubleTestArg1"));
+        double arg2 = Double.parseDouble(resourcesReader.getCalculatorArgument("subDoubleTestArg2"));
+        assertEquals("Expected value is not equal to actual", expectedResult, calculator.sub(arg1, arg2), DELTA);
     }
 
     @Category({CalculatorTestSuite.class, SmokeTestsSuite.class})
     @Test
-    public void multLongTest() {
-        assertEquals("Expected value is not equal to actual", 4611686018427387904L, calculator.mult(2147483648L, 2147483648L));
+    public void multLongTest() throws IOException {
+        long expectedResult = Long.parseLong(resourcesReader.getCalculatorArgument("multLongTestExpected"));
+        long arg = Long.parseLong(resourcesReader.getCalculatorArgument("multLongTestArg"));
+        assertEquals("Expected value is not equal to actual", expectedResult, calculator.mult(arg, arg));
     }
 
     @Category({CalculatorTestSuite.class, SmokeTestsSuite.class, FailingTestsSuite.class})
     @Test
-    public void multDoubleTest() {
-        final double DELTA = 1e-15;
-        assertEquals("Expected value is not equal to actual", 11.0922, calculator.mult(2.78, 3.99), DELTA);
+    public void multDoubleTest() throws IOException {
+        final double DELTA = Double.parseDouble(resourcesReader.getCalculatorArgument("delta"));
+        double expectedResult = Double.parseDouble(resourcesReader.getCalculatorArgument("multDoubleTestExpected"));
+        double arg1 = Double.parseDouble(resourcesReader.getCalculatorArgument("multDoubleTestArg1"));
+        double arg2 = Double.parseDouble(resourcesReader.getCalculatorArgument("multDoubleTestArg2"));
+        assertEquals("Expected value is not equal to actual", expectedResult, calculator.mult(arg1, arg2), DELTA);
     }
 
     @Category({CalculatorTestSuite.class, SmokeTestsSuite.class})
     @Test
-    public void divDoubleTest() {
-        final double DELTA = 1e-15;
-        assertEquals("Expected value is not equal to actual", 4.0, calculator.div(20.0, 5.0), DELTA);
+    public void divDoubleTest() throws IOException {
+        final double DELTA = Double.parseDouble(resourcesReader.getCalculatorArgument("delta"));
+        double expectedResult = Double.parseDouble(resourcesReader.getCalculatorArgument("divDoubleTestExpected"));
+        double arg1 = Double.parseDouble(resourcesReader.getCalculatorArgument("divDoubleTestArg1"));
+        double arg2 = Double.parseDouble(resourcesReader.getCalculatorArgument("divDoubleTestArg2"));
+        assertEquals("Expected value is not equal to actual", expectedResult, calculator.div(arg1, arg2), DELTA);
     }
 
     @Category({CalculatorTestSuite.class, FailingTestsSuite.class, ExceptionTestSuite.class})
     @Test (expected = NumberFormatException.class)
-    public void divDoubleExceptionTest() {
-        calculator.div(20.0, 0.0);
+    public void divDoubleExceptionTest() throws IOException {
+        double arg1 = Double.parseDouble(resourcesReader.getCalculatorArgument("divDoubleExceptionTestArg1"));
+        double arg2 = Double.parseDouble(resourcesReader.getCalculatorArgument("divDoubleExceptionTestArg2"));
+        calculator.div(arg1, arg2);
     }
 
     @Category({CalculatorTestSuite.class, SmokeTestsSuite.class})
     @Test
-    public void divLongTest() {
-        assertEquals("Expected value is not equal to actual", 4L, calculator.div(20L, 5L));
+    public void divLongTest() throws IOException {
+        long expectedResult = Long.parseLong(resourcesReader.getCalculatorArgument("divLongTestExpected"));
+        long arg1 = Long.parseLong(resourcesReader.getCalculatorArgument("divLongTestArg1"));
+        long arg2 = Long.parseLong(resourcesReader.getCalculatorArgument("divLongTestArg2"));
+        assertEquals("Expected value is not equal to actual", expectedResult, calculator.div(arg1, arg2));
     }
 
     @Category({CalculatorTestSuite.class, ExceptionTestSuite.class})
     @Test (expected = NumberFormatException.class)
-    public void divLongExceptionTest() {
-        calculator.div(20L, 0L);
+    public void divLongExceptionTest() throws IOException {
+        long arg1 = Long.parseLong(resourcesReader.getCalculatorArgument("divLongExceptionTestArg1"));
+        long arg2 = Long.parseLong(resourcesReader.getCalculatorArgument("divLongExceptionTestArg2"));
+        calculator.div(arg1, arg2);
     }
 
     @Category({CalculatorTestSuite.class, SmokeTestsSuite.class, FailingTestsSuite.class})
     @Test
-    public void pow() {
-        final double DELTA = 1e-5;
-        assertEquals("Expected value is not equal to actual", 247.684822619, calculator.pow(3.5, 4.4), DELTA);
+    public void pow() throws IOException {
+        final double DELTA = Double.parseDouble(resourcesReader.getCalculatorArgument("delta"));
+        double expectedResult = Double.parseDouble(resourcesReader.getCalculatorArgument("powExpected"));
+        double arg1 = Double.parseDouble(resourcesReader.getCalculatorArgument("powArg1"));
+        double arg2 = Double.parseDouble(resourcesReader.getCalculatorArgument("powArg2"));
+        assertEquals("Expected value is not equal to actual", expectedResult, calculator.pow(arg1, arg2), DELTA);
     }
 
     @Category({CalculatorTestSuite.class, SmokeTestsSuite.class})
     @Test
-    public void sqrtDoubleTest() {
-        final double DELTA = 1e-15;
-        assertEquals("Expected value is not equal to actual", 8.8, calculator.sqrt(77.44), DELTA);
+    public void sqrtDoubleTest() throws IOException {
+        final double DELTA = Double.parseDouble(resourcesReader.getCalculatorArgument("delta"));
+        double expectedResult = Double.parseDouble(resourcesReader.getCalculatorArgument("sqrtDoubleTestExpected"));
+        double arg = Double.parseDouble(resourcesReader.getCalculatorArgument("sqrtDoubleTestArg"));
+        assertEquals("Expected value is not equal to actual", expectedResult, calculator.sqrt(arg), DELTA);
     }
 
     @Category({CalculatorTestSuite.class, FailingTestsSuite.class})
     @Test
-    public void tg() {
-        final double DELTA = 1e-5;
-        assertEquals("Expected value is not equal to actual", Math.tan(45), calculator.tg(45), DELTA);
+    public void tg() throws IOException {
+        final double DELTA = Double.parseDouble(resourcesReader.getCalculatorArgument("delta"));
+        double arg = Double.parseDouble(resourcesReader.getCalculatorArgument("tgArg"));
+        assertEquals("Expected value is not equal to actual", Math.tan(arg), calculator.tg(arg), DELTA);
     }
 
     @Category({CalculatorTestSuite.class, FailingTestsSuite.class})
     @Test
-    public void ctg() {
-        final double DELTA = 1e-15;
-        assertEquals("Expected value is not equal to actual", (1.0/Math.tan(0.5)), calculator.ctg(0.5), DELTA);
+    public void ctg() throws IOException {
+        final double DELTA = Double.parseDouble(resourcesReader.getCalculatorArgument("delta"));
+        double arg = Double.parseDouble(resourcesReader.getCalculatorArgument("ctgArg"));
+        assertEquals("Expected value is not equal to actual", (1.0/Math.tan(arg)), calculator.ctg(arg), DELTA);
     }
 
     @Category({CalculatorTestSuite.class, FailingTestsSuite.class})
     @Test
-    public void cos() {
-        final double DELTA = 1e-15;
-        assertEquals("Expected value is not equal to actual", (Math.cos(45)), calculator.cos(45), DELTA);
+    public void cos() throws IOException {
+        final double DELTA = Double.parseDouble(resourcesReader.getCalculatorArgument("delta"));
+        double arg = Double.parseDouble(resourcesReader.getCalculatorArgument("cosArg"));
+        assertEquals("Expected value is not equal to actual", (Math.cos(arg)), calculator.cos(arg), DELTA);
     }
 
     @Category({CalculatorTestSuite.class})
     @Test
-    public void sin() {
-        final double DELTA = 1e-15;
-        assertEquals("Expected value is not equal to actual", (Math.sin(45)), calculator.sin(45), DELTA);
+    public void sin() throws IOException {
+        final double DELTA = Double.parseDouble(resourcesReader.getCalculatorArgument("delta"));
+        double arg = Double.parseDouble(resourcesReader.getCalculatorArgument("sinArg"));
+        assertEquals("Expected value is not equal to actual", (Math.sin(arg)), calculator.sin(arg), DELTA);
     }
 
     @Category({CalculatorTestSuite.class, SmokeTestsSuite.class})
     @Test
-    public void isPositiveTrueTest() {
-        assertTrue("Actual value isn't true", calculator.isPositive(4294967296L));
+    public void isPositiveTrueTest() throws IOException {
+        long arg = Long.parseLong(resourcesReader.getCalculatorArgument("isPositiveTrueTestArg"));
+        assertTrue("Actual value isn't true", calculator.isPositive(arg));
     }
 
     @Category({CalculatorTestSuite.class, SmokeTestsSuite.class})
     @Test
-    public void isPositiveFalseTest() {
-        assertFalse("Actual value isn't false", calculator.isPositive(-4294967296L));
+    public void isPositiveFalseTest() throws IOException {
+        long arg = Long.parseLong(resourcesReader.getCalculatorArgument("isPositiveFalseTestArg"));
+        assertFalse("Actual value isn't false", calculator.isPositive(arg));
     }
 
     @Category({CalculatorTestSuite.class, FailingTestsSuite.class})
     @Test
-    public void isPositiveZeroTest() {
-        assertTrue("Actual value isn't true", calculator.isPositive(-0L));
+    public void isPositiveZeroTest() throws IOException {
+        long arg = Long.parseLong(resourcesReader.getCalculatorArgument("isPositiveZeroTestArg"));
+        assertTrue("Actual value isn't true", calculator.isPositive(arg));
     }
 
     @Category({CalculatorTestSuite.class, SmokeTestsSuite.class})
     @Test
-    public void isNegativeTrueTest() {
-        assertTrue("Actual value isn't true", calculator.isNegative(-2));
+    public void isNegativeTrueTest() throws IOException {
+        long arg = Long.parseLong(resourcesReader.getCalculatorArgument("isNegativeTrueTestArg"));
+        assertTrue("Actual value isn't true", calculator.isNegative(arg));
     }
 
     @Category({CalculatorTestSuite.class, SmokeTestsSuite.class})
     @Test
-    public void isNegativeFalseTest() {
-        assertFalse("Actual value isn't false", calculator.isNegative(4294967296L));
+    public void isNegativeFalseTest() throws IOException {
+        long arg = Long.parseLong(resourcesReader.getCalculatorArgument("isNegativeFalseTestArg"));
+        assertFalse("Actual value isn't false", calculator.isNegative(arg));
     }
 
     @Category({CalculatorTestSuite.class})
     @Test
-    public void isNegativeZeroTest() {
-        assertFalse("Actual value isn't false", calculator.isNegative(0L));
+    public void isNegativeZeroTest() throws IOException {
+        long arg = Long.parseLong(resourcesReader.getCalculatorArgument("isNegativeZeroTest"));
+        assertFalse("Actual value isn't false", calculator.isNegative(arg));
     }
 
 }
